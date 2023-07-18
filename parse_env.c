03c3a33bd17e62eef2788282527d2660e04a93d1
Zakaria El Bouzkri <zel-bouz@e3r3p6.1337.ma>
2023-07-18 05:47:32 +0100

    Tue Jul 18 05:47:32 +01 2023

--- parse_env.c
+++ parse_env.c
@@ -6,7 +6,7 @@
 /*   By: zel-bouz <zel-bouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 11:52:53 by asettar           #+#    #+#             */
-/*   Updated: 2023/07/18 04:21:59 by zel-bouz         ###   ########.fr       */
+/*   Updated: 2023/07/18 05:40:40 by zel-bouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,18 @@ void	ft_env_add_back(t_env **env, t_env *node)
 	}
 	ft_env_add_back(&(*env)->next, node);
 }
+
+t_env	*ft_new_env(char *name, char *value)
+{
+	t_env	*new;
+
+	new = malloc(sizeof(t_env));
+	ft_memset(new, 0, sizeof(t_env));
+	new->value = value;
+	new->name = name;
+	return (new);
+}
+
 
 void	parse_env(char **envp, t_env **env)
 {
@@ -44,6 +56,10 @@ void	parse_env(char **envp, t_env **env)
 		node->next = NULL;
 		ft_env_add_back(env, node);
 	}
+	ft_env_delete(env, env_find("PWD", *env));
+	ft_env_delete(env, env_find("OLDPWD", *env));
+	ft_env_add_back(env, ft_new_env(ft_strdup("PWD"), getcwd(NULL, 0)));
+	ft_env_add_back(env, ft_new_env(ft_strdup("OLDPWD"), NULL));
 }
 
 void	free_env(t_env	**env)
